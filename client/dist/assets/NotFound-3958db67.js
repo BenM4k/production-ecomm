35db62e6f7b5a181a0322c77a5dc00c39ca4a246
BenM4k <bennymakomo@gmail.com>
2023-11-24 08:21:51 +0200

    Second build

---
+++ client/dist/assets/NotFound-3958db67.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-f99a0d8e.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
---
+++ client/dist/assets/Store-1488fa4e.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-f99a0d8e.js";const e=()=>r.jsx("div",{children:"Store"});export{e as default};
