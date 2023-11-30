9419e24a5f75070b9a0618cee9f59289832f9a3f
BenM4k <bennymakomo@gmail.com>
2023-11-30 10:20:38 +0200

    Added vercel front link

--- server/config/corsOptions.ts
+++ server/config/corsOptions.ts
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://localhost:3001',
-    'http://102.22.141.170'
+    'http://102.22.141.170',
+    'https://production-ecomm.vercel.app/'
 ];
 
 const corsOptions = {
