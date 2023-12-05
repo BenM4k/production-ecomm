fbc62e6c03fb95d3a8579712aa13abae60a66d90
BenM4k <bennymakomo@gmail.com>
2023-12-05 13:23:42 +0200

    Builded the app

--- client/dist/assets/NotFound-3958db67.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-f99a0d8e.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
--- client/dist/assets/SignUp-e094024e.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-f99a0d8e.js";const i=()=>r.jsx("div",{children:"SignUp"});export{i as default};
